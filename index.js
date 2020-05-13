bffb68d83895284a02ccf609347ff89440ac2baf
vansh <vanshkapoorvk7@gmail.com>
2020-05-14 03:13:52 +0530

    first graphql schema and api made

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 
+// Allows express to understand graphQL
+const expressGraphQL = require("express-graphql");
+
+const schema = require("./schema");
+
 const app = express();
 
-app.listen(5000, () => {
+// expressgraphql middleware knows how to understand the graphql request
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema: schema,
+    graphiql: true
+  })
+);
+
+app.listen(3000, () => {
   console.log("server is running");
 });
---
+++ schema.js
@@ -0,0 +1,43 @@
+// @FILE NOTE : This file is the schema for the graphQL
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// making dummy data
+var books = [
+  { name: "book1", genre: "adv", id: "1" },
+  { name: "book2", genre: "fictional", id: "2" },
+  { name: "book3", genre: "sci fi", id: "3" }
+];
+
+//TASK1 :  DEFINING THE SCHEMA
+const BookType = new GraphQLObjectType({
+  name: "Book", //name for the schema object
+  fields: () => ({
+    //This is a function because it helps the mutuple types that will be created in future to  interaxt with each other
+    id: { type: GraphQLString }, //The complete shcema is made of types that are understandable by the graphQL
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+//TASK2 :  DEFINING THE QUERIES ON THE SCHEMA JUST MADE
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    //THE style you define this field, is the way how you will get the data
+    book: {
+      type: BookType, //name of the schema
+      args: { id: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id });
+        // code to get data from db / other resources
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
